b98090bcd2ffbe74f95989ec82fbb562248c7792
krishivagarwal <krishiv.agarwal@cgpworldwide.com>
2025-06-07 17:36:49 +0530

    Imports and readme brief

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smart_portfolio_optimiser
+
+A python-based sharpe ratio optimiser for US portfolio investments.
---
+++ main.py
@@ -0,0 +1,10 @@
+# Imports
+import yfinance
+import pandas as pd
+import numpy
+import scipy.optimize
+import matplotlib.pyplot
+
+# Constants
+
+# Taking Ticker Data 
